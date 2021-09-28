4c3feb6dc3191bcb9fdc0e0f562745d477d3964f
HyperLife1119 <hyperlife1119@qq.com>
2021-09-28 16:20:03 +0800

    feat(types): add type Property and Method

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,3 +1,9 @@
 export interface Type<T> extends Function {
   new(...args: any[]): T;
 }
+
+/** 过滤方法 */
+export type Property<O extends object> = Omit<O, { [K in keyof O]: O[K] extends Function ? K : never }[keyof O]>;
+
+/** 过滤属性 */
+export type Method<O extends object> = Omit<O, keyof Property<O>>;
