e39729fb10bd6b552ce17825806be3e624e1be5b
HyperLife1119 <hyperlife1119@qq.com>
2021-10-01 23:36:02 +0800

    fix(http): fix the `append` method of http-headers

--- packages/http/src/headers.ts
+++ packages/http/src/headers.ts
@@ -1,6 +1,7 @@
 export class HttpHeaders {
+  /** lowercase name => values */
   private headers: Map<string, string[]> = new Map<string, string[]>();
-  /** lowercase name => normalized name */
+  /** 以 lowercase name => normalized name 的形式维护一份 header name */
   private normalizedNames: Map<string, string> = new Map();
 
   constructor(headers?: { [name: string]: string | string[] }) {
@@ -15,48 +16,72 @@ export class HttpHeaders {
     });
   }
 
+  /**
+   * 向现有的值中添加新的值
+   * @param name
+   * @param value
+   * @returns
+   */
   append(name: string, value: string | string[]): HttpHeaders {
     const key = name.toLowerCase();
     const base = this.headers.get(key) || [];
 
+    if (!Array.isArray(value)) {
+      value = [value];
+    }
+
     base.push(...value);
     this.headers.set(key, base);
     this.normalizedNames.has(key) || this.normalizedNames.set(key, name);
-    return this;
+    return this.clone();
   }
 
-  delete(name: string) {
+  /**
+   * 通过给定标头从现有值中删除一个值
+   * @param name
+   * @returns
+   */
+  delete(name: string): HttpHeaders {
     const key = name.toLowerCase();
     this.headers.delete(key);
     this.normalizedNames.delete(key);
-    return this;
+    return this.clone();
   }
 
-  get(name: string) {
+  /**
+   * 通过给定标头获取第一个值
+   * @param name
+   * @returns
+   */
+  get(name: string): string {
     const values = this.headers.get(name.toLowerCase());
     return values && values.length > 0 ? values[0] : null;
   }
 
-  getAll(name: string) {
+  /**
+   * 通过给定标头获取所有值
+   * @param name
+   * @returns
+   */
+  getAll(name: string): string[] {
     return this.headers.get(name.toLowerCase()) || null;
   }
 
-  has(name: string) {
+  /**
+   * 检查给定标头是否存在
+   * @param name
+   * @returns
+   */
+  has(name: string): boolean {
     return this.headers.has(name.toLowerCase());
   }
 
-  forEach(fn: (name: string, value: string[]) => void) {
-    this.headers.forEach((value, name) => fn(this.normalizedNames.get(name), value));
-  }
-
-  keys() {
-    return this.normalizedNames.values();
-  }
-
-  values() {
-    return this.headers.values();
-  }
-
+  /**
+   * 设置一个值并覆盖已有值
+   * @param name
+   * @param value
+   * @returns
+   */
   set(name: string, value: string | string[]): HttpHeaders {
     const key = name.toLowerCase();
 
@@ -66,6 +91,25 @@ export class HttpHeaders {
 
     this.headers.set(key, value);
     this.normalizedNames.has(key) || this.normalizedNames.set(key, name);
-    return this;
+    return this.clone();
+  }
+
+  forEach(fn: (name: string, value: string[]) => void): void {
+    this.headers.forEach((value, name) => fn(this.normalizedNames.get(name), value));
+  }
+
+  keys(): IterableIterator<string> {
+    return this.normalizedNames.values();
+  }
+
+  private clone(): HttpHeaders {
+    const clone = new HttpHeaders();
+
+    this.headers.forEach((value, name) => {
+      clone.headers.set(name, value);
+      clone.normalizedNames.set(name, this.normalizedNames.get(name));
+    });
+
+    return clone;
   }
 }
