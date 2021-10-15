9b45756251a44690b582d7caa8e3d8fc45408428
HyperLife1119 <hyperlife1119@qq.com>
2021-10-15 12:31:42 +0800

    refactor(http): 使用 `HttpContextToken` 以区分是 HTTP 请求还是 WX 文件上传

--- packages/http/src/backends/wx.ts
+++ packages/http/src/backends/wx.ts
@@ -1,14 +1,25 @@
 import { Observable, Observer } from 'rxjs';
 import { HttpBackend } from '../backend';
+import { HttpContextToken } from '../context';
 import { HttpHeaders } from '../headers';
 import { HttpRequest } from '../request';
 import { HttpResponse } from '../response';
 
-const isOk = (statusCode: number) => statusCode >= 200 && statusCode < 300;
+const isOk = (status: number) => status >= 200 && status < 300;
+
+/** 携带此 token 的 post 请求，将使用 wx.uploadFile() 进行文件上传 */
+export const WX_UPLOAD_FILE_TOKEN = new HttpContextToken(() => ({
+  filePath: '',
+  fileName: ''
+}));
 
 export class WxHttpBackend implements HttpBackend {
   handle(request: HttpRequest<any>): Observable<HttpResponse<any>> {
     return new Observable((observer: Observer<any>) => {
+      if (request.method === 'PATCH') {
+        throw Error('WeChat MiniProgram does not support http method as ' + request.method);
+      }
+
       const complete = () => observer.complete();
       const error = (error: any) => observer.error(new HttpResponse(
         request.url,
@@ -23,18 +34,15 @@ export class WxHttpBackend implements HttpBackend {
         headers[name] = value.join(',');
       });
 
-      if (request.method === 'UPLOAD') {
-        const { filePath, fileName } = request.data;
-
-        delete request.data.filePath;
-        delete request.data.fileName;
+      if (request.method === 'POST' && request.context.has(WX_UPLOAD_FILE_TOKEN)) {
+        const { filePath, fileName } = request.context.get(WX_UPLOAD_FILE_TOKEN);
 
         wx.uploadFile({
-          url: request.url,
+          url: request.urlWithParams,
           filePath: filePath,
           name: fileName,
           header: headers,
-          formData: request.data,
+          formData: request.body,
           timeout: request.timeout,
           success: ({ data, statusCode }) => {
             const response = new HttpResponse(
@@ -55,9 +63,9 @@ export class WxHttpBackend implements HttpBackend {
       }
 
       wx.request({
-        url: request.url,
+        url: request.urlWithParams,
         method: request.method,
-        data: request.data,
+        data: request.body,
         header: headers,
         responseType: request.responseType,
         dataType: request.dataType as 'json' | '其他',
