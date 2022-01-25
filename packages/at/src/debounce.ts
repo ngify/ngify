7cebd08ee5a27c7064b81c189fd687b88f48e22f
HyperLife1119 <hyperlife1119@qq.com>
2022-01-25 16:35:25 +0800

    fix(at): fix bug

--- packages/at/src/debounce.ts
+++ packages/at/src/debounce.ts
@@ -1,18 +1,18 @@
 import { SafeAny } from '@ngify/types';
 
-const map = new WeakMap();
-
 /**
  * Debounce decorator for class methods
  * @param wait
  */
 export function Debounce(wait: number): MethodDecorator {
+  const map = new WeakMap();
+
   return (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<SafeAny>) => {
     const fn = descriptor.value as (...args: SafeAny[]) => void;
 
     descriptor.value = function (...args: SafeAny[]) {
-      map.has(fn) && clearTimeout(map.get(fn));
-      map.set(fn, setTimeout(() => fn.apply(this, args), wait));
+      map.has(this) && clearTimeout(map.get(this));
+      map.set(this, setTimeout(() => fn.apply(this, args), wait));
     };
   }
 }
--- packages/at/src/throttle.ts
+++ packages/at/src/throttle.ts
@@ -1,19 +1,19 @@
 import { SafeAny } from '@ngify/types';
 
-const map = new WeakMap();
-
 /**
  * Throttle decorator for class methods
  * @param wait
  */
 export function Throttle(wait: number): MethodDecorator {
+  const map = new WeakMap();
+
   return (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<SafeAny>) => {
     const fn = descriptor.value as (...args: SafeAny[]) => void;
 
     descriptor.value = function (...args: SafeAny[]) {
-      map.has(fn) || map.set(fn, setTimeout(() => {
+      map.has(this) || map.set(this, setTimeout(() => {
         fn.apply(this, args);
-        map.delete(fn);
+        map.delete(this);
       }, wait));
     };
   }
